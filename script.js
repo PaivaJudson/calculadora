be2e68358f3c8c2740ed7738acdd4f3d2ab5593f
Judson Paiva <judson.paiva@hotmail.com>
2023-05-01 22:15:26 +0100

    calculadora

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function calcular(){
     var operacao = document.getElementById('operacao').value;
     
     var num1 = document.getElementById('num1').value;
-    
     var num2 = document.getElementById('num2').value;
     
     //forçando o tipo de variável
@@ -10,12 +9,12 @@ function calcular(){
     num2 = parseFloat(num2);
     
     if(num1 == '' || num1 == null){
-        alert('Favor digitar um número válido para operação (num1)');
+        alert('Favor digitar um número válido para operação ');
         return false;
     }
     
     if(num2 == '' || num2 == null){
-        alert('Favor digitar um número válido para operação (num2)');
+        alert('Favor digitar um número válido para operação ');
         return false;
     }
     
